01893a6660d96b72c49a1e8a7e7682be6e3e9fbd
ranyacmp <ranyacmp2018@gmail.com>
2023-01-15 20:20:24 -0300

    Resolvido questao 27 sequenciais

--- SEQUENCIAIS/QUESTAO27.c
+++ SEQUENCIAIS/QUESTAO27.c
@@ -5,3 +5,21 @@
     
     36800 segundos são 10 horas e 13 minutos e 20 segundos
 */
+
+#include <stdio.h> 
+
+main(){
+
+    int segundos;
+    printf("Digite um valor em segundos: \n");
+    scanf("%d", &segundos);
+
+    int horas = segundos / 3600; 
+    int reshoras= segundos % 3600;
+
+    int minutos= reshoras / 60;
+    int seg= reshoras % 60;
+
+    printf("%d segundos sao %d horas %d minutos e %d segundos \n", segundos, horas, minutos, seg);
+    return 0; 
+}
