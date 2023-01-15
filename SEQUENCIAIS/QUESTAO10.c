2fab43749947cb604df77e962ae12198b653c0d9
ranyacmp <ranyacmp2018@gmail.com>
2023-01-15 01:47:53 -0300

    Resolvido questao 10 sequenciais

--- SEQUENCIAIS/QUESTAO10.c
+++ SEQUENCIAIS/QUESTAO10.c
@@ -1,3 +1,22 @@
 /*
 10) Escreva um programa que leia um número inteiro e determine se ele é par ou ímpar.
 */
+
+#include <stdio.h> 
+
+main(){
+
+    int num;
+    printf("Digite um numero: \n");
+    scanf("%d",&num);
+
+    if((num % 2) == 0){
+        printf("Esse numero eh par. \n");
+    }else{
+        printf("Esse numero eh impar. \n");
+    }
+
+
+
+    return 0;
+}
