3beaefc4a8532fcd312fc69dc42769eb7ce99f77
ranyacmp <ranyacmp2018@gmail.com>
2023-01-15 18:40:48 -0300

    Resolvido questao 23 sequenciais

--- SEQUENCIAIS/QUESTAO23.c
+++ SEQUENCIAIS/QUESTAO23.c
@@ -4,3 +4,25 @@
     - O produto do primeiro número pelo quadrado do segundo;
     - O resto da divisão do primeiro número pelo segundo
 */
+
+#include <stdio.h> 
+
+main(){
+
+
+    int num1; 
+    printf("Digite um numero: \n");
+    scanf("%d", &num1);
+
+    int num2; 
+    printf("Digite um numero: \n");
+    scanf("%d", &num2);
+
+    printf("A soma dos numeros eh: %d.\n", (num1 + num2));
+
+    printf("O produto do primeiro pelo segundo eh: %d \n", (num1 * num2));
+
+    printf("O resto da divisao do primeiro pelo segundo eh: %d \n",(num1 % num2));
+
+    return 0; 
+}
