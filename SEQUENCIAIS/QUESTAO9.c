222e98cf07b027b7be05cdc726b06020281bb49d
ranyacmp <ranyacmp2018@gmail.com>
2023-01-15 01:45:20 -0300

    Resolvido questao 9 sequenciais

--- SEQUENCIAIS/QUESTAO9.c
+++ SEQUENCIAIS/QUESTAO9.c
@@ -1,3 +1,26 @@
 /*
 9) Escreva um programa que leia dois números e exiba o maior deles.
 */
+
+#include <stdio.h> 
+
+main(){
+
+    int num1; 
+    printf("Digite um numero: \n");
+    scanf("%d",&num1);
+
+    int num2; 
+    printf("Digite um numero: \n");
+    scanf("%d",&num2);
+
+    if(num1 > num2){
+        printf("O maior numero eh: %d \n", num1);
+    }else if(num2 > num1){
+        printf("O maior numero eh : %d \n ", num2);
+    }else{
+        printf("Os numeros sao iguais. \n ");
+    }
+
+    return 0; 
+}
