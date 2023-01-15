f5428bcc7283443fe5c5fe7fe11e3e623dec4b72
ranyacmp <ranyacmp2018@gmail.com>
2023-01-15 19:51:34 -0300

    Resolvido questao 25 sequenciais

--- SEQUENCIAIS/QUESTAO25.c
+++ SEQUENCIAIS/QUESTAO25.c
@@ -3,3 +3,36 @@
 as dimensões de uma casa (também em metros e retangular) sobre este terreno. Em seguida calcule 
 e apresente a área livre do terreno, em metros quadrados e em percentagem.
 */
+
+#include <stdio.h> 
+
+main(){
+
+    float larguraT;
+    printf("Digite a largura o terreno: \n");
+    scanf("%f", &larguraT);
+
+    float compriT;
+    printf("Digite o comprimento do terreno: \n");
+    scanf("%f", &compriT);
+
+    float larguraC;
+    printf("Digite a largura da casa: \n");
+    scanf("%f", &larguraC);
+
+    float compriC;
+    printf("Digite o comprimento da casa: \n");
+    scanf("%f", &compriC);
+
+    float areaT= larguraT * compriT;
+    float areaC= larguraC * compriC;
+
+    float areaLM= areaT - areaC;
+    float porcentagem=(100 * areaLM) / areaT;
+
+    printf("%.2f metros de area livre, representam %.2f porcento do terreno. \n", areaLM, porcentagem);
+
+
+
+    return 0; 
+}
