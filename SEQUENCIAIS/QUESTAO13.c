0ed20eaea793733bedfd3df052b7858e0fc2ec1e
ranyacmp <ranyacmp2018@gmail.com>
2023-01-15 02:15:43 -0300

    Resolvido questao 13 sequenciais

--- SEQUENCIAIS/QUESTAO13.c
+++ SEQUENCIAIS/QUESTAO13.c
@@ -1,3 +1,22 @@
 /*
 13) Entrar com um número e informar se ele é divisível por 14 e por 17, ao mesmo tempo.
 */
+
+
+#include <stdio.h> 
+
+main(){
+
+
+    int num; 
+    printf("Digite um numero: \n");
+    scanf("%d", &num);
+
+    if((num % 14) == 0 && (num % 17) == 0){
+        printf("Esse numero eh divisivel por 14 e 17 ao mesmo tempo. \n");
+    }else{
+        printf("Esse numero nao eh divisivel por 14 e 17 ao mesmo tempo. \n");
+    }
+
+    return 0; 
+}
