4688b0587a2f39f1a136ea696c0f7dc9ad35656b
ranyacmp <ranyacmp2018@gmail.com>
2023-01-15 02:11:28 -0300

    Resolvido questao 12 sequenciais

--- SEQUENCIAIS/QUESTAO12.c
+++ SEQUENCIAIS/QUESTAO12.c
@@ -2,3 +2,25 @@
 12) Fazer um programa em "C" que leia o preço de um produto. Se o preço for menor 
 que R$500,00, conceda um desconto de 8,7%. Acima deste valor, conceda um desconto de 10%.
 */
+
+#include <stdio.h> 
+
+main(){
+
+    float preco;
+    printf("Digite o preco do produto: \n");
+    scanf("%f", &preco);
+
+    float porcentagem;
+    if(preco < 500){
+        porcentagem= (preco * 8.7) / 100;
+    }else{
+        porcentagem= (preco * 10) / 100;
+    }
+    float total= preco - porcentagem; 
+
+    printf("O valor total com desconto eh de : %.2f \n", total);
+
+
+    return 0;
+}
