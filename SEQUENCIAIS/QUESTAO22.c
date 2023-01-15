917c9e434ee11caa3975a2b4d833c20b665358e1
ranyacmp <ranyacmp2018@gmail.com>
2023-01-15 18:26:02 -0300

    Resolvido questao 22 sequenciais

--- SEQUENCIAIS/QUESTAO22.c
+++ SEQUENCIAIS/QUESTAO22.c
@@ -3,3 +3,21 @@
 dias trabalhados pelo encanador e imprima a quantia líquida que deverá ser paga, sabendo-se que são 
 descontados 8% para imposto de renda.
 */
+
+#include <stdio.h> 
+
+main(){
+
+    int dias; 
+    printf("Digite a quantidade de dias trabalhados: \n");
+    scanf("%d", &dias);
+
+    float bruto= dias * 30; 
+    float imposto= (bruto * 8) / 100;
+    float total= bruto - imposto;
+
+    printf("O valor a ser pago eh: %.2f reais. \n", total); 
+
+
+    return 0; 
+}
