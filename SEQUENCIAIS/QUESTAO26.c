bada775e38dde207c799bb6b8d00a4bbce04def0
ranyacmp <ranyacmp2018@gmail.com>
2023-01-15 20:07:49 -0300

    Resolvido questao 26 sequenciais

--- SEQUENCIAIS/QUESTAO26.c
+++ SEQUENCIAIS/QUESTAO26.c
@@ -34,3 +34,33 @@ usado seja o menor possível.
         0 notas de 5     → 3/5 = 0; 3%5 = 3
         3 notas de 1    → 3 lá de cima
 */
+
+#include <stdio.h>
+
+main(){
+
+    int valor; 
+    printf("Digite um valor em reai: \n");
+    scanf("%d", &valor);
+
+   int cem = valor / 100;
+   int rescem = valor % 100;
+
+   int cinquenta= rescem / 50; 
+   int rescinquenta= rescem % 50;
+
+   int dez= rescinquenta / 10;
+   int resdez= rescinquenta % 10;
+
+   int cinco= resdez / 5;
+   int rescinco= resdez % 5;
+
+
+    printf("%d notas de 100 \n", cem);
+    printf("%d notas de 50 \n",cinquenta);
+    printf("%d notas de 10 \n", dez);
+    printf("%d notas de 5 \n", cinco);
+    printf("%d notas de 1 \n", rescinco);
+    
+    return 0;
+}
