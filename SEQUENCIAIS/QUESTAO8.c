9949d69df20f888557f859712d66050cf5d478d5
ranyacmp <ranyacmp2018@gmail.com>
2023-01-15 01:41:35 -0300

    Resolvido questao 8 sequenciais

--- SEQUENCIAIS/QUESTAO8.c
+++ SEQUENCIAIS/QUESTAO8.c
@@ -11,3 +11,30 @@ utilizada na viagem com a
 fórmula: LITROS_USADOS = DISTÂNCIA / X. O programa deverá apresentar os valores
 
 */
+
+#include <stdio.h> 
+
+main(){
+
+    float kmL;
+    printf("Digite quantos km/Litro o carro faz: \n");
+    scanf("%f", &kmL);
+    
+    float tempo; 
+    printf("Digite o tempo gasto na viagem: \n");
+    scanf("%f", &tempo);
+
+    float velocidade;
+    printf("Digite a velocidade media durante a viagem: \n");
+    scanf("%f", &velocidade);
+
+    float distancia= tempo * velocidade; 
+    
+    printf("A distancia percorrida foi: %.2f \n", distancia);
+
+    float qtdL= distancia / kmL; 
+
+    printf("Foram utilizados %.2f litros na viagem. \n", qtdL);
+
+    return 0;
+}
