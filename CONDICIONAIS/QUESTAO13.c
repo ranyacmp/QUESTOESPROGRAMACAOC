cc4652d42465525839241be515f8a10d6a8796ae
ranyacmp <ranyacmp2018@gmail.com>
2023-02-02 20:51:30 -0300

    Resolvido questao 13 condicionais

--- CONDICIONAIS/QUESTAO13.c
+++ CONDICIONAIS/QUESTAO13.c
@@ -7,3 +7,42 @@ Se não houver, exiba uma mensagem de erro.
     - Se Δ = 0 existe uma raiz real. Imprima a raiz e a mensagem "Raiz  única" e a raiz.
     - Se Δ < 0 não existe raiz real. Imprima a mensagem "Não existe raiz".
 */
+
+#include <stdio.h> 
+#include <math.h> 
+
+main(){
+
+    float a;
+    printf("Digite um valor para a: \n");
+    scanf("%f", &a);
+
+    float b;
+    printf("Digite um valor para b: \n");
+    scanf("%f", &b);
+
+    float c; 
+    printf("Digite um valor para c: \n");
+    scanf("%f", &c);
+
+
+    float delta=0, raiz=0; 
+
+    if(a == 0 || b == 0 || c == 0){
+        printf("Equacao incompleta. \n");
+        return;
+    }else{
+        delta= (pow(b, 2) * (4 * (a) * (c)));
+        if(delta == 0){
+            printf("Raiz unica. \n");
+            raiz= ((-b) + delta) / (2 * a); 
+            pintf("Raiz: %.2f", raiz);
+        }else if(delta < 0){
+            printf("Nao existe raiz real. \n"); 
+        }
+    }
+
+
+
+    return 0; 
+}
