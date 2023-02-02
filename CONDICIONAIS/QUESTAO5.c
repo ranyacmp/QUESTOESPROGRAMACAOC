4b5b8ec427e8146101f2d31dcd0d2ed06f3836e2
ranyacmp <ranyacmp2018@gmail.com>
2023-02-01 21:02:22 -0300

    Resolvido questao 5 condicionais

--- CONDICIONAIS/QUESTAO5.c
+++ CONDICIONAIS/QUESTAO5.c
@@ -5,3 +5,45 @@ média e uma das mensagens: Aprovado , Reprovado ou AV3 (a média é 6.0 para ap
     - Vai para AV3 se média de AV1 e AV2 for menor que 6.
     - A AV3 substitui a menor nota
 */
+
+
+#include <stdio.h>
+
+main(){
+
+    float av1;
+    printf("Digite a nota da av1: \n");
+    scanf("%f", &av1);
+
+    float av2;
+    printf("Digite a nota da av2: \n");
+    scanf("%f", &av2);
+
+
+   float media= (av1 + av2) / 2;
+
+   printf("NOTA AV1: %.2f", av1);
+   printf("NOTA AV2: %.2f", av2);
+   printf("MEDIA: %.2f", media);
+
+   if(media < 6){
+
+        printf("REPROVADO. \n");
+
+        if(av1 > av2){
+            printf("A av3 ira substituir a nota da av2. \n ");
+        }else if(av2 > av1){
+            printf("A av3 ira substituir a nota da av1. \n ");
+        }else{
+            printf("A av3 substituira qualquer uma das notas. \n ");
+        }
+
+   }else{
+
+        printf("APROVADO. \n"); 
+
+   }
+
+    return 0; 
+
+}
