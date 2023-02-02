09557d0fd9678cb9b7e0dfce308d7b9fbf89f12a
ranyacmp <ranyacmp2018@gmail.com>
2023-02-01 21:33:27 -0300

    Resolvido questao 9 condicionais

--- CONDICIONAIS/QUESTAO9.c
+++ CONDICIONAIS/QUESTAO9.c
@@ -3,3 +3,27 @@
 se a prestação for maior que 20% do salário imprima: Empréstimo não concedido, caso contrário imprima: 
 Empréstimo concedido
 */
+
+#include <stdio.h>
+
+
+main(){
+
+    float salario; 
+    printf("Digite o valor do salario: \n ");
+    scanf("%f", &salario);
+
+    float emprestimo;
+    printf("Digite o valor do emprestimo: \n ");
+    scanf("%f", &emprestimo);
+
+    float percent= (salario * 20) / 100;
+
+    if(emprestimo > percent){
+        printf("Emprestimo nao concedido. \n");
+    }else{
+        printf("Emprestimo concedido. \n");
+    }
+
+    return 0;
+}
