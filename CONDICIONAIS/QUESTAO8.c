125b38fc6942109203ae459de49db0b73852fe65
ranyacmp <ranyacmp2018@gmail.com>
2023-02-01 21:23:26 -0300

    Resolvido questao 8 condicionais

--- CONDICIONAIS/QUESTAO8.c
+++ CONDICIONAIS/QUESTAO8.c
@@ -1,3 +1,22 @@
 /*
 8) Construir um programa que indique se o número digitado está compreendido entre 20 e 90 ou não.
 */
+
+#include <stdio.h>
+
+
+main(){
+
+    float num; 
+    printf("Digite um numero: \n");
+    scanf("%f", &num);
+
+    if(num >= 20 && num <= 90){
+        printf("Esse numero esta compreendido entre 20 e 90. \n");
+    }else{
+        printf("Esse numero nao esta compreendido entre 20 e 90. \n");
+    }
+
+    
+    return 0; 
+}
