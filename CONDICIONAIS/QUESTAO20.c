3095943857a7eb87df38fe42236f117d627d7189
ranyacmp <ranyacmp2018@gmail.com>
2023-02-02 23:19:51 -0300

    Resolvido questao 20 condicionais

--- CONDICIONAIS/QUESTAO20.c
+++ CONDICIONAIS/QUESTAO20.c
@@ -5,3 +5,44 @@ e mostre na tela a pergunta: qual é a soma de ”a”+ ”b”, onde ”a”e �
 números aleatórios. Peça resposta. Faça cinco perguntas ao aluno, e mostre para 
 ele as perguntas e as respostas corretas, além de quantas vezes o aluno acertou.
 */
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+main(){
+
+    int num1=0, num2=0, acertos=0, erros=0, condicao=0; 
+    while(condicao < 5){
+
+        num1=0;
+        num2=0;
+        for(int i=0; i < 2; i++){
+
+            if(num1 == 0){
+                num1= rand() % 100;
+            }else{
+                num2= rand() % 50;
+            }
+
+        }
+
+        int resposta;
+        printf("Qual o valor da soma: %d + %d ? \n", num1, num2);
+        scanf("%d", &resposta);
+
+        if(resposta == (num1 + num2)){
+            printf("Acertou! \n");
+            acertos++; 
+        }else{
+            printf("Errou! \n");
+            erros++;
+        }
+
+        condicao++;
+    }
+
+    printf("O aluno teve %d acertos e %d erros. \n", acertos, erros);
+
+    return 0;
+}
