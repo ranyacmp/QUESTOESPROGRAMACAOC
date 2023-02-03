edb04abb7c9bb916c42b2468db0d3f3891619fe1
ranyacmp <ranyacmp2018@gmail.com>
2023-02-02 21:17:12 -0300

    Resolvido questao 16 condicionais

--- CONDICIONAIS/QUESTAO16.c
+++ CONDICIONAIS/QUESTAO16.c
@@ -5,3 +5,27 @@ não se aposentar. As condições para aposentadoria são
     - Ou ter trabalhado pelo menos 30 anos,
     - Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos.
 */
+
+#include <stdio.h> 
+
+main(){
+    
+    int tempo; 
+    printf("Digite o tempo que voce trabalhou: \n");
+    scanf("%d", &tempo);
+
+    int idade; 
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    if(idade >= 65 || tempo >= 30){
+        printf("Voce ja pode se aposentar. \n");
+    }else if(idade >= 60 || tempo >= 25){
+        printf("Voce ja pode se aposentar. \n");
+    }else{
+        printf("Voce ainda nao pode se aposentar. \n");
+    }
+
+
+    return 0; 
+}
