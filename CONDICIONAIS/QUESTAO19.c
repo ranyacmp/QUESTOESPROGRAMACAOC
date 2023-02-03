65ffdc0caae2d9daa063a114e322079404c07285
ranyacmp <ranyacmp2018@gmail.com>
2023-02-02 21:46:26 -0300

    Resolvido questao 19 condicionais

--- CONDICIONAIS/QUESTAO19.c
+++ CONDICIONAIS/QUESTAO19.c
@@ -7,3 +7,29 @@ menor que	8	Venda o carro!
 entre	8 e 14	Econômico
 maior que	14	Super econônico
 */
+
+#include <stdio.h> 
+
+main(){
+
+    float distancia; 
+    printf("Digite a diatancia percorrida: \n");
+    scanf("%f", &distancia);
+
+    float litros;
+    printf("Digite a quantidade de litros de gasolina consumidos: \n");
+    scanf("%f",&litros);
+
+    float consumo = distancia / litros; 
+
+    if(consumo < 8){
+        printf("Venda o carro ! \n");
+    }else if(consumo >= 8 && consumo <= 14){
+        printf("Economico. \n");
+    }else if(consumo > 14){
+        printf("Super economico. \n");
+    }
+
+    return 0; 
+
+}
