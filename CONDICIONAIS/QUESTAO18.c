302e5763d98052c2c2e56058b63da8ad69a14fd3
ranyacmp <ranyacmp2018@gmail.com>
2023-02-02 21:38:50 -0300

    Resolvido questao 18 condicionais

--- CONDICIONAIS/QUESTAO18.c
+++ CONDICIONAIS/QUESTAO18.c
@@ -3,3 +3,50 @@
 imprima o dia da semana correspondente a este numero. Isto  é, domingo, 
 se 1, segunda-feira, se 2, e assim por diante.
 */
+
+#include <stdio.h>
+
+main(){
+
+    int num;
+    printf("Digite um numero de 1 a 7: \n");
+    scanf("%d", &num);
+
+    switch (num){
+
+        case 1:
+        printf("Domingo \n");
+        break;
+
+        case 2:
+        printf("Segunda feira \n");
+        break;
+
+        case 3:
+        printf("Terca feira \n");
+        break;
+
+        case 4:
+        printf("Quarta feira \n");
+        break;
+
+        case 5:
+        printf("Quinta feira \n");
+        break;
+
+        case 6:
+        printf("Sexta feira \n");
+        break;
+
+        case 7:
+        printf("Sabado \n");
+        break;
+    
+        default:
+            printf("Numero nao reconhecido. \n");
+            break;
+    }
+
+    
+    return 0; 
+}
