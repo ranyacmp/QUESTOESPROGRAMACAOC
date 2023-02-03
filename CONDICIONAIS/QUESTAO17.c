527ddf184327ecbfc94ebf1bbc41565f7f33d0e2
ranyacmp <ranyacmp2018@gmail.com>
2023-02-02 21:33:52 -0300

    Resolvido questao 17 condicionais

--- CONDICIONAIS/QUESTAO17.c
+++ CONDICIONAIS/QUESTAO17.c
@@ -7,3 +7,63 @@ não bissextos.
     Um ano é bissexto se o ano é divisível por 400. Ou se o ano é divisível por 
     4 e não é divisível por 100
 */
+
+#include <stdio.h> 
+
+main(){
+
+    int dia; 
+    printf("Digite o dia: \n");
+    scanf("%d", &dia);
+
+    int mes; 
+    printf("Digite o mes. \n");
+    scanf("%d", &mes);
+
+    int ano; 
+    printf("Digite o ano. \n");
+    scanf("%d", &ano);
+
+    
+
+        if(mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12){
+            
+            if(dia >= 1 && dia <= 31){
+                printf("Data valida. \n");
+            }else{
+                printf("Data invalida. \n");
+            }
+
+        }else if(mes == 4 || mes == 6 || mes == 9 || mes == 11){
+
+            if(dia >= 1 && dia <= 30){
+                printf("Data valida. \n");
+            }else{
+                printf("Data invalida. \n");
+            }
+
+        }else if(mes == 2 && ((ano % 400) == 0)){
+
+            if( dia >= 1 && dia <= 29){
+                printf("Data valida. \n");
+            }else{
+                printf("Data invalida. \n");
+            }
+
+        }else if(mes == 2 && ((ano % 400) != 0)){
+
+            if( dia >= 1 && dia <= 28){
+                printf("Data valida. \n");
+            }else{
+                printf("Data invalida. \n");
+            }
+            
+        }else{
+            printf("Data invalida. \n");
+        }
+
+   
+
+    
+    return 0; 
+}
