e7e607ffd569fa861731dd27f4c1b224fcd0cbf6
ranyacmp <ranyacmp2018@gmail.com>
2023-02-02 21:54:09 -0300

    Removendo arquivos sobrando

---
+++ CONDICIONAIS/QUESTAO20.c
@@ -0,0 +1,7 @@
+/*
+21. Faça  uma prova de matemática para crianças que estão aprendendo a somar 
+números inteiros menores do que cem. Escolha números aleatórios entre 1 e 100, 
+e mostre na tela a pergunta: qual é a soma de ”a”+ ”b”, onde ”a”e ”b” são os 
+números aleatórios. Peça resposta. Faça cinco perguntas ao aluno, e mostre para 
+ele as perguntas e as respostas corretas, além de quantas vezes o aluno acertou.
+*/
--- CONDICIONAIS/QUESTAO21.c
+++ CONDICIONAIS/QUESTAO21.c
@@ -1,7 +1,18 @@
 /*
-21. Faça  uma prova de matemática para crianças que estão aprendendo a somar 
-números inteiros menores do que cem. Escolha números aleatórios entre 1 e 100, 
-e mostre na tela a pergunta: qual é a soma de ”a”+ ”b”, onde ”a”e ”b” são os 
-números aleatórios. Peça resposta. Faça cinco perguntas ao aluno, e mostre para 
-ele as perguntas e as respostas corretas, além de quantas vezes o aluno acertou.
+22. As tarifas de certo parque de estacionamento são as seguintes: 
+    - 1a e 2a hora - R$ 1,00 cada
+    - 3a e 4a hora - R$ 1,40 cada
+    - 5a hora e seguintes - R$ 2,00 cada
+    
+    O número de horas a pagar é sempre inteiro e arredondado por excesso. Deste modo, 
+    quem estacionar durante 61 minutos pagará  por duas horas, que é o mesmo que pagaria 
+    se tivesse permanecido 120 minutos. Os momentos de chegada ao parque e partida deste 
+    são apresentados na forma de pares de inteiros, representando horas e minutos. Por 
+    exemplo, o par 12 15 representará ”meio dia e quinze”. 
+    
+    Pretende-se um programa que, lidos pelo teclado os momentos de chegada e de partida, 
+    escreva na tela o preço cobrado pelo estacionamento. Deverá haver validação de dados. 
+    Admite-se que a chegada e a partida se dão com intervalo não superior a 24 horas. 
+    Isto significa que se houver uma hora de chegada 14 30 e hora de partida 12 00 significa 
+    que o carro ficou no estacionamento por 22 horas e 30 minutos.
 */
